a6070dfeac80f9d7f453a9029be17fb49254c9c5
Jacob <jacob.peat01@gmail.com>
2022-03-13 14:17:24 +0000

    add event listener for instructions button and close instructions button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,7 @@
 const instructionsBtn = document.querySelector(".instructions-btn");
 const instructionsContainer = document.querySelector(".instructions");
 const closeInstructions = document.getElementById("close-instructions")
+
+// Event Listeners
+instructionsBtn.addEventListener("click", openInstructions);
+closeInstructions.addEventListener("click", closeInstructionsContainer);

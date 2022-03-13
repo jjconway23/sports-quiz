19383bfcc7256b4601a2e6d5926d56c5a056f3cb
Jacob <jacob.peat01@gmail.com>
2022-03-13 14:17:56 +0000

    program open instructions function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,8 @@ const closeInstructions = document.getElementById("close-instructions")
 // Event Listeners
 instructionsBtn.addEventListener("click", openInstructions);
 closeInstructions.addEventListener("click", closeInstructionsContainer);
+
+//opens instructions
+function openInstructions() {
+    instructionsContainer.style.display = "block";
+}

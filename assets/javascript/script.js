585490932975d8b1bef3375642499be9ad5a152b
Jacob <jacob.peat01@gmail.com>
2022-03-13 14:16:05 +0000

    add dom selectors for the homepage

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+// Dom Selectors
+const instructionsBtn = document.querySelector(".instructions-btn");
+const instructionsContainer = document.querySelector(".instructions");
+const closeInstructions = document.getElementById("close-instructions")

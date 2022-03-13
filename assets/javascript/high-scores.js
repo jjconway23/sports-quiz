2b81dac857104d6b7624fc9db7ed7092aa2ea1d4
Jacob <jacob.peat01@gmail.com>
2022-03-13 14:43:21 +0000

    add list score and username as list items on high score page

--- assets/javascript/high-scores.js
+++ assets/javascript/high-scores.js
@@ -1,2 +1,5 @@
 const highScoreList = document.getElementById("high-scores-list");
 const highScores = JSON.parse( localStorage.getItem("highScores")) || [];
+highScoreList.innerHTML = highScores.map( score => {
+    return `<li class= "score-items">${score.name} : ${score.score}</li>`
+}).join("")

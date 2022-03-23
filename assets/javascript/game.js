9a77b40987a4a2699e944b2dff2f1b13f8cd189a
Jacob <jacob.peat01@gmail.com>
2022-03-23 13:22:42 +0000

    rearrange variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,33 @@
-let answerBtn = Array.from(document.getElementsByClassName("answer-btn"));
-let presentQuestion = {}; 
-let acceptingAnswers = false;
-let counter = 0;
-let remainingQuestions = [];
-const maxQuestions = 11 ;
-const topFive = 5;
-let questionIncrement = 1;
-let playerScore = 0;
-let questions = [];
-let questionNumber = 0;
-
-// dom selectors
-
 const startGameBtn = document.getElementById("start-game");
 const welcomePage = document.getElementById("welcome-page");
 const gameNav = document.querySelector(".game-nav");
 const questionsDiv = document.getElementById("questions-div")
 const timer = document.getElementById("timer");
-let gameScore = document.querySelectorAll(".game-score")[0];
-let gameScoreFinale = document.querySelector(".results-score");
-let quizQuestionNumber = document.getElementById("quiz-question-number");
-let quizQuestion = document.getElementById("quiz-question");
+const gameScore = document.querySelectorAll(".game-score")[0];
+const gameScoreFinale = document.querySelector(".results-score");
+const quizQuestionNumber = document.getElementById("quiz-question-number");
+const quizQuestion = document.getElementById("quiz-question");
 const playAgainButton = document.getElementById("play-again-button");
 const resultsPage = document.getElementById("results-page");
-let progress = document.getElementById("progress");
-let loader = document.getElementById("loader");
-let gamesContainer = document.querySelector(".games-container");
-// quiz question buttons
-let optionOne = document.getElementById("option-1-btn");
-let optionTwo = document.getElementById("option-2-btn");
-let optionThree = document.getElementById("option-3-btn");
-let optionFour = document.getElementById("option-4-btn");
-let nextBtn = document.getElementById("next-btn");
-let checkResultsBtn = document.getElementById("check-results");
+const progress = document.getElementById("progress");
+const loader = document.getElementById("loader");
+const gamesContainer = document.querySelector(".games-container");
+const nextBtn = document.getElementById("next-btn");
+const checkResultsBtn = document.getElementById("check-results");
+const answerBtn = Array.from(document.getElementsByClassName("answer-btn"));
+const maxQuestions = 11 ;
+const topFive = 5;
+let questions = [];
+let presentQuestion = {}; 
+let acceptingAnswers = false;
+let counter = 0;
+let remainingQuestions = [];
+let questionIncrement = 1;
+let playerScore = 0;
+let questionNumber = 0;
+
+
+
 
 // Game Page Event Listeners
 startGameBtn.addEventListener("click", startGame);
